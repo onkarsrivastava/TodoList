52ca13c660ba9caf6f331bba3b3ca65d2d573630
Onkar Srivastava <onkarsrivastava@Onkars-MacBook-Pro.local>
2024-09-18 01:18:03 +0530

    TodoList using JS DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task')
+
+addTask.addEventListener
